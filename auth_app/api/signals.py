372cc9dbd7cd2461dd316fbb5c26180bd4dafdc3
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2025-04-29 16:43:05 +0200

    Registers API signals on app ready

---
+++ auth_app/api/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from profiles_app.models import Profile
+
+
+@receiver(post_save, sender=Profile)
+def edit_user_after_profile_update(sender, instance, **kwargs):
+    user = instance.user
+    user.first_name = instance.first_name
+    user.last_name = instance.last_name
+    user.email = instance.email
+    user.save()
