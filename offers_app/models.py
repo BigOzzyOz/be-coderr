f017f52c2c2e22c6814de73b96a005ecbbbf48ef
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2025-05-02 03:40:11 +0200

    Adds Offer and OfferDetail models with fields and relationships; updates Profile model to include file upload path and type choices; removes unused test files and views.

--- offers_app/models.py
+++ offers_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class OfferDetailModel(models.Model):
+    OFFER_TYPE_CHOICES = [
+        ("basic", "Basic"),
+        ("standard", "Standard"),
+        ("premium", "Premium"),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    revisions = models.CharField(max_length=255, blank=True, null=True)
+    delivery_time_in_days = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    features = models.JSONField(default=list, blank=True, null=True)
+    offer_type = models.CharField(choices=OFFER_TYPE_CHOICES, max_length=50, blank=True)
+    offer = models.ForeignKey("OfferModel", on_delete=models.CASCADE, related_name="offer_detail", default=None)
+
+    def __str__(self):
+        return self.title
+
+
+class OfferModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="offers")
+    title = models.CharField(max_length=255)
+    image = models.FileField(upload_to="offers/", blank=True, null=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Offer by {self.user.username} for {self.offer_detail.title}"
