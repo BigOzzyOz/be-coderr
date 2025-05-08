af20aabc8d29afba97e32ca6715deff5f8956c4d
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2025-05-08 16:00:35 +0200

    Refactor authentication views: streamline RegisterView and LoginView by replacing post method with create method, enhancing error handling and code clarity.

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -1,7 +1,6 @@
 from rest_framework.authtoken.models import Token
 from django.db import transaction
 from rest_framework.generics import CreateAPIView
-from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
@@ -12,51 +11,37 @@ class RegisterView(CreateAPIView):
     permission_classes = [AllowAny]
     serializer_class = RegisterSerializer
 
-    def post(self, request):
-        try:
-            serializer = self.get_serializer(data=request.data)
-
-            if serializer.is_valid():
-                with transaction.atomic():
-                    type_value = serializer.validated_data["type"]
-                    user = serializer.save()
-                    user.profile.type = type_value
-                    user.profile.save()
-                    token, _ = Token.objects.get_or_create(user=user)
-                    data = {
-                        "token": token.key,
-                        "username": user.username,
-                        "email": user.email,
-                        "user_id": user.id,
-                    }
-                    return Response(data, status=status.HTTP_201_CREATED)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        except Exception as e:
-            return Response({"detail": "Internal server error."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        with transaction.atomic():
+            type_value = serializer.validated_data["type"]
+            user = serializer.save()
+            user.profile.type = type_value
+            user.profile.save()
+            token, _ = Token.objects.get_or_create(user=user)
+            data = {
+                "token": token.key,
+                "username": user.username,
+                "email": user.email,
+                "user_id": user.id,
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
 
 
-class LoginView(ObtainAuthToken):
+class LoginView(CreateAPIView):
     permission_classes = [AllowAny]
     serializer_class = LoginSerializer
 
-    def post(self, request):
-        try:
-            serializer = self.get_serializer(data=request.data)
-
-            if serializer.is_valid():
-                user = serializer.validated_data["user"]
-                token, _ = Token.objects.get_or_create(user=user)
-                data = {
-                    "token": token.key,
-                    "username": user.username,
-                    "email": user.email,
-                    "user_id": user.id,
-                }
-                return Response(data, status=status.HTTP_200_OK)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        except Exception as e:
-            return Response({"detail": "Internal server error."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, _ = Token.objects.get_or_create(user=user)
+        data = {
+            "token": token.key,
+            "username": user.username,
+            "email": user.email,
+            "user_id": user.id,
+        }
+        return Response(data, status=status.HTTP_200_OK)
